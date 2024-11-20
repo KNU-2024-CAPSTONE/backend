c8b623e717f9056c94809a1f8b7484dce40e0fad
sunha park <sunhapark1004@gmail.com>
2024-11-20 16:03:59 +0900

    쇼핑몰 db 테스트
    500 에러

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO shop (shopId, shopName, shopUrl) VALUES ('shop1', 'Shop One', 'https://localhost:8081/api/database');
+INSERT INTO shop (shopId, shopName, shopUrl) VALUES ('shop2', 'Shop Two', 'https://shop2.com');
+INSERT INTO shop (shopId, shopName, shopUrl) VALUES ('shop3', 'Shop Three', 'https://shop3.com');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE shop (
+                      shopId VARCHAR(50) PRIMARY KEY,
+                      shopName VARCHAR(255) NOT NULL,
+                      shopUrl VARCHAR(255) NOT NULL
+);
