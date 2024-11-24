cfec86390f0c90698a7d26eec9135a9a330f5f4b
2iedo <162278702+2iedo@users.noreply.github.com>
2024-11-24 15:32:36 +0900

    refactor : 사용하지 않는 라이브러리 제거

--- src/main/java/knu/project/crm/sale/dto/SaleForRecommendResponse.java
+++ src/main/java/knu/project/crm/sale/dto/SaleForRecommendResponse.java
@@ -1,7 +1,5 @@
 package knu.project.crm.sale.dto;
 
-import java.time.LocalDateTime;
-
 public record SaleForRecommendResponse(String gender, int age, Product product, int quantity) {
     public record Product(String name, int price){}
 }
